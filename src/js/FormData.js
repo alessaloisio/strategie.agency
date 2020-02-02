5a306cf72ed0f6d42c2791a041803b6aa0e959ff
Alessandro Aloisio <alessaloisio@gmail.com>
2020-02-02 15:53:35 +0100

    clean all error message

--- src/js/FormData.js
+++ src/js/FormData.js
@@ -6,9 +6,23 @@ export default class FormData {
     this.submit = false;
 
     this.idErrorBlock = "error-message";
+    this.classErrorInput = "error-message";
 
     // INIT FORM
+    this.cleanAll();
     this.getAll();
+  }
+
+  cleanAll() {
+    this.form.querySelector(`#${this.idErrorBlock}`).innerText = "";
+    Array.from(this.form.querySelectorAll("input")).map(input => {
+      if (input.parentNode) {
+        const child = input.parentNode.querySelector(
+          `.${this.classErrorInput}`
+        );
+        if (child) input.parentNode.removeChild(child);
+      }
+    });
   }
 
   getAll() {
@@ -34,29 +48,42 @@ export default class FormData {
 
   request(action, method = "GET") {
     return new Promise(resolve => {
-      if (!Object.keys(this.errors).length)
-        fetch(action, {
-          method: method,
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(this.data)
-        })
-          .then(response => response.json())
-          .then(response => {
-            if (response.type === "error") {
-              console.log(response);
-              if (response.message) {
-                document.getElementById(this.idErrorBlock).innerText =
-                  response.message;
-              } else if (response.data) {
-              }
-            } else {
-              this.submit = true;
-            }
+      fetch(action, {
+        method: method,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.data)
+      })
+        .then(response => response.json())
+        .then(response => {
+          if (response.type === "error") {
+            console.log(response);
 
-            resolve(true);
-          });
+            if (response.message) {
+              // Error with the database (multi-participation)
+              document.getElementById(this.idErrorBlock).innerText =
+                response.message;
+            } else if (response.data) {
+              // Error with fields
+              const data = response.data;
+              Object.keys(data).forEach(key => {
+                // Add error message field
+                const element = this.form.querySelector(`[name=${key}]`);
+                if (element.parentNode) {
+                  let p = document.createElement("p");
+                  p.classList.add(this.classErrorInput);
+                  p.innerText = data[key];
+                  element.parentNode.appendChild(p);
+                }
+              });
+            }
+          } else if (response.type === "success") {
+            this.submit = true;
+          }
+
+          resolve(true);
+        });
     });
   }
 }
